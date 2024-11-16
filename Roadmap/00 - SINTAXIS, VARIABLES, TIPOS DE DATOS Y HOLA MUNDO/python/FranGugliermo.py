0409779b7ec46546dbb45309d23e50d2590e8090
TuNombre <tuemail@example.com>
2024-11-15 23:16:46 -0300

    Primer commit

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/FranGugliermo.py
@@ -0,0 +1,47 @@
+# ¿Preparad@ para aprender o repasar el lenguaje de programación que tú quieras?
+# Recuerda que todas las instrucciones de participación están en el
+# repositorio de GitHub.
+
+# Lo primero... ¿Ya has elegido un lenguaje?
+# No todos son iguales, pero sus fundamentos suelen ser comunes.
+# Este primer reto te servirá para familiarizarte con la forma de participar
+# enviando tus propias soluciones.
+
+# EJERCICIO:
+# Crea un comentario en el código y coloca la URL del sitio web oficial del
+# lenguaje de programación que has seleccionado. 
+# Representa las diferentes sintaxis que existen de crear comentarios
+# en el lenguaje (en una línea, varias...).
+# Crea una variable (y una constante si el lenguaje lo soporta).
+# Crea variables representando todos los tipos de datos primitivos
+# del lenguaje (cadenas de texto, enteros, booleanos...).
+# Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!" 
+# ¿Fácil? No te preocupes, recuerda que esta es una ruta de estudio y debemos comenzar por el principio.
+ 
+
+# URL oficial del lenguaje seleccionado: https://www.python.org/
+
+#Comentario de una línea
+
+"""Comentario de 
+múltiples líneas"""
+
+'''Otro comentario de 
+múltiples líneas'''
+
+#Creando una variable
+variable1 = "" #Creamos una variable con una cadena vacía.
+
+#Python no puede crear constantes, pero por convención se utiliza el nombre de las variables en MAYÚSCULAS para representarlas.
+NUMERO_PI = 3.14159
+
+#Creando variables con datos primitivos
+cadena = "Soy una cadena"
+cadena_2 = 'Soy otra cadena'
+entero = 10
+flotante = 10.0
+booleano = True
+nulo = None
+
+#Imprimiendo en pantalla ¡Hola, Python!
+print("¡Hola, Python!")
